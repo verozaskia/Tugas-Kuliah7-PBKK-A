0f13d10444c954181abeb31efaac1db1f512d242
verozaskia <zqholby@gmail.com>
2023-10-09 21:35:18 +0700

    first commit

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Maui.Controls;
+using System;
+
+namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        int clickCount = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnCounterClicked(object sender, EventArgs e)
+        {
+            clickCount++;
+            CounterLabel.Text = $"Clicked {clickCount} times";
+        }
+
+        void OnSayHelloClicked(object sender, EventArgs e)
+        {
+            string name = NameEntry.Text;
+            if (!string.IsNullOrWhiteSpace(name))
+            {
+                GreetingLabel.Text = $"Hello, {name}!";
+            }
+        }
+    }
+}
